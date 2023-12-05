afa7731442effb6f810b2b4358ae3bd433bbff4f
eloiselcr <eloise.lecronier@gmail.com>
2023-12-05 10:59:04 +0100

    - ajout exercice 28

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+/*import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Boop! <code>Llama</code> boop 🦙.
         </p>
         <a
           className="App-link"
@@ -23,3 +23,50 @@ function App() {
 }
 
 export default App;
+*/
+
+
+// Exercice 28
+
+import React, { useState } from 'react';
+
+// Composant RandomColorText
+const RandomColorText = () => {
+  // Utilisation du state pour stocker la couleur actuelle
+  const [textColor, setTextColor] = useState('#000000');
+
+  // Fonction pour générer une couleur aléatoire au format hexadécimal
+  const getRandomColor = () => {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+
+  // Gestionnaire pour changer la couleur lorsqu'on survole le texte
+  const handleMouseOver = () => {
+    // Génère une nouvelle couleur aléatoire
+    const newColor = getRandomColor();
+    // Met à jour le state avec la nouvelle couleur
+    setTextColor(newColor);
+  };
+
+  // Rendu du composant
+  return (
+    <div>
+      {/* Utilisation du composant h1 avec le style dynamique en fonction de textColor */}
+      <h1
+        style={{ color: textColor }}
+        // Ajout d'un gestionnaire d'événements pour l'événement onMouseOver
+        onMouseOver={handleMouseOver}
+      >
+        Texte avec couleur aléatoire
+      </h1>
+    </div>
+  );
+};
+
+export default RandomColorText;
+
